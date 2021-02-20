2111244c2cd0636049ac590974dcae7f1edc579b
dennerpl <denner.souzapl@gmail.com>
2021-02-20 08:59:59 -0300

    Updates

---
+++ precache-manifest.57af4c1c015efd2d378ecbbdf10eb123.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d0d099c73ff4505e387261346999cf7",
+    "url": "/trivia-react-redux/index.html"
+  },
+  {
+    "revision": "4a9c3fa1f0759aea9809",
+    "url": "/trivia-react-redux/static/css/main.8f5d9c73.chunk.css"
+  },
+  {
+    "revision": "529610619fab58713365",
+    "url": "/trivia-react-redux/static/js/2.4cc34526.chunk.js"
+  },
+  {
+    "revision": "5d1b8067085602e259fd0862f57df793",
+    "url": "/trivia-react-redux/static/js/2.4cc34526.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4a9c3fa1f0759aea9809",
+    "url": "/trivia-react-redux/static/js/main.4d48db2d.chunk.js"
+  },
+  {
+    "revision": "c9265d93de189acb100f",
+    "url": "/trivia-react-redux/static/js/runtime-main.460e2238.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/trivia-react-redux/static/media/trivia.466d153e.png"
+  }
+]);
